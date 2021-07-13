1f50eb915de0b4f39b0055d9160e09bdf923f9ab
sleepturtle2 <sayantan.mukh98@gmail.com>
2021-07-13 16:29:43 +0530

    changed title

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -4,9 +4,9 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>FireGram</h1>
+      <h1>Photo Gallery</h1>
       <h2>Your Pictures</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Store your uploaded pictures here!</p>
     </div>
   )
 }
